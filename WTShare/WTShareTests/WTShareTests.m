c87a960946d72dad3b3dedf984241ef1f846a1ee
coderWu <codewutong@foxmail.com>
2016-07-02 15:52:17 +0800

    初始化项目

---
+++ WTShare/WTShareTests/WTShareTests.m
@@ -0,0 +1,39 @@
+//
+//  WTShareTests.m
+//  WTShareTests
+//
+//  Created by Mac on 16/7/2.
+//  Copyright © 2016年 wutong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WTShareTests : XCTestCase
+
+@end
+
+@implementation WTShareTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
